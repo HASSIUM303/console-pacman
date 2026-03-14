ab9d37de181eef4c162e93616b56763fbb248925
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-03-14 17:26:07 +0800

    Чтение и вывод содержимого файла карты

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ class Program
 {
    static void Main()
    {
-      
+      char[,] map = null;
+
+      string[] file = File.ReadAllLines("map.txt");
+
+      foreach (var line in file)
+         Console.WriteLine(line);
    }
 }
