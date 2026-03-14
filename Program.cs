2fdc658d8633c5c0a40ffc12b70bc3c32edb71bc
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-03-14 17:54:17 +0800

    Исправление логики чтения карты: изменение порядка индексов в массиве и добавление метода для отрисовки карты

--- Program.cs
+++ Program.cs
@@ -5,18 +5,28 @@ class Program
 {
    static void Main()
    {
-
+      char[,] map = ReadMap("map.txt");
+      DrawMap(map);
    }
    private static char[,] ReadMap(string path)
    {
       string[] file = File.ReadAllLines("map.txt");
-      char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+      char[,] map = new char[file.Length, GetMaxLengthOfLine(file)];
 
       for (int x = 0; x < map.GetLength(0); x++)
          for (int y = 0; y < map.GetLength(1); y++)
-            map[x, y] = file[y][x];
+            map[x, y] = file[x][y];
 
       return map;
+   }
+   public static void DrawMap(char[,] map)
+   {
+      for (int x = 0; x < map.GetLength(0); x++)
+      {
+         for (int y = 0; y < map.GetLength(1); y++)
+            Console.Write(map[x, y]);
+         Console.WriteLine();
+      }
    }
    private static int GetMaxLengthOfLine(string[] lines)
    {
