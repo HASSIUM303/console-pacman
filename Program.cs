0eab1d83426a95ab6bd293d798cc771a9023c424
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-03-14 17:35:33 +0800

    Добавление метода для чтения карты и определения максимальной длины строки

--- Program.cs
+++ Program.cs
@@ -5,11 +5,21 @@ class Program
 {
    static void Main()
    {
-      char[,] map = null;
-
+      
+   }
+   private static char[,] ReadMap(string path)
+   {
       string[] file = File.ReadAllLines("map.txt");
+      char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+   }
+   private static int GetMaxLengthOfLine(string[] lines)
+   {
+      int maxLength = lines[0].Length;
 
-      foreach (var line in file)
-         Console.WriteLine(line);
+      foreach (var line in lines)
+         if (line.Length > maxLength)
+            maxLength = line.Length;
+
+      return maxLength;
    }
 }
